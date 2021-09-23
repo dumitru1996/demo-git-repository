443e10072a31a1029577b24b7b5d4375b5c7fdcf
dbodac <dumitru.bodac@endava.com>
2021-09-23 11:43:43 +0300

    Create another pull request

--- demo/src/test/java/TestClass.java
+++ demo/src/test/java/TestClass.java
@@ -2,7 +2,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-
+        System.out.println("Let's make a pull request");
         System.out.println("Let's make a pull request");
     }
 }
